994074a6d7059f26e38e88f80c31782a2fdcb74a
Micheal Lanham <cxbxmxcx@gmail.com>
2024-03-08 19:35:30 -0700

    Fixes to agent function calling

--- gpt_nexus/chat_base/actions/test_actions.py
+++ gpt_nexus/chat_base/actions/test_actions.py
@@ -4,11 +4,11 @@ from chat_base.action_manager import agent_action
 @agent_action
 def get_current_weather(location, unit="fahrenheit"):
     """Get the current weather in a given location"""
-    return f"The current weather in {location} is 45{unit}."
+    return f"The current weather in {location} is 0 {unit}."
 
 
 # Example usage for a semantic action
 @agent_action
 def recommend(topic):
     """Provide a recommendation for a given {{topic}}."""
-    return __doc__
+    return "Stellaris"
--- gpt_nexus/chat_base/agents/oai_agent.py
+++ gpt_nexus/chat_base/agents/oai_agent.py
@@ -1,3 +1,4 @@
+import json
 import time
 
 from chat_base.agent_manager import BaseAgent
@@ -39,7 +40,7 @@ class OpenAIAgent(BaseAgent):
         self.last_message = str(response)
         return str(response)
 
-    def get_response_stream(self, user_input, thread_id=None):
+    def get_response_stream_old(self, user_input, thread_id=None):
         self.last_message = ""
         self.messages += [{"role": "user", "content": user_input}]
         response = self.client.chat.completions.create(
@@ -48,6 +49,58 @@ class OpenAIAgent(BaseAgent):
             temperature=0.7,
         )
         self.last_message = str(response.choices[0].message.content)
+
+        def generate_responses():
+            response = self.last_message
+            for character in response:
+                time.sleep(0.01)
+                yield character
+
+        return generate_responses
+
+    def get_response_stream(self, user_input, thread_id=None):
+        self.last_message = ""
+        self.messages += [{"role": "user", "content": user_input}]
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=self.messages,
+            tools=self.tools,
+            tool_choice="auto",  # auto is default, but we'll be explicit
+        )
+        response_message = response.choices[0].message
+        tool_calls = response_message.tool_calls
+        # Step 2: check if the model wanted to call a function
+        if tool_calls:
+            # Step 3: call the function
+            # Note: the JSON response may not always be valid; be sure to handle errors
+            available_functions = {
+                action["name"]: action["pointer"] for action in self.actions
+            }
+            self.messages.append(
+                response_message
+            )  # extend conversation with assistant's reply
+            # Step 4: send the info for each function call and function response to the model
+            for tool_call in tool_calls:
+                function_name = tool_call.function.name
+                function_to_call = available_functions[function_name]
+                function_args = json.loads(tool_call.function.arguments)
+                function_response = function_to_call(**function_args)
+
+                self.messages.append(
+                    {
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": function_name,
+                        "content": function_response,
+                    }
+                )  # extend conversation with function response
+            second_response = self.client.chat.completions.create(
+                model=self.model,
+                messages=self.messages,
+            )  # get a new response from the model where it can see the function response
+            response_message = second_response.choices[0].message
+
+        self.last_message = str(response_message.content)
 
         def generate_responses():
             response = self.last_message
