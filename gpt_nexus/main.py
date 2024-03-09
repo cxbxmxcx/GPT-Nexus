d30a37c3f07884727bba070908da4b04a98675c2
Micheal Lanham <cxbxmxcx@gmail.com>
2024-03-08 19:47:43 -0700

    Update to start file paths

--- gpt_nexus/main.py
+++ gpt_nexus/main.py
@@ -1,11 +1,13 @@
+import os
 import subprocess
 
 
 def run():
-    # Define the command to run your Streamlit app.
-    command = ["streamlit", "run", "gpt_nexus/streamlit_ui.py"]
+    # Determine the directory of the current file (e.g., the script this function is in)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # Construct the path to streamlit_ui.py relative to this directory
+    app_path = os.path.join(dir_path, "gpt_nexus", "streamlit_ui.py")
 
-    # Start the Streamlit app as a subprocess.
+    command = ["streamlit", "run", app_path]
     process = subprocess.Popen(command)
-
     process.wait()
